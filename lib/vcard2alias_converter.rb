6801e3aa498576a39f2fee29f94c4b3bf68a211c
Marcus Ahnve <marcus@ahnve.com>
2008-10-03 23:17:26 +0200

    converts one address

--- lib/vcard2alias_converter.rb
+++ lib/vcard2alias_converter.rb
@@ -1,5 +1,27 @@
+require 'vpim/vcard'
+
 class Vcard2aliasConverter
   def convert(input)
-    'user_usersson User Usersson user@usersson.com'
+    card = Vpim::Vcard.decode(input).first
+    alias_line = ""
+    alias_line << create_nick(card)
+    alias_line << " "
+    alias_line << card.name.given
+    alias_line << " "
+    alias_line << card.name.family
+    alias_line << ' ' 
+    alias_line << card.email('HOME')
+    alias_line
   end
+
+  private
+
+  def create_nick(card)
+    nick = ""
+    nick << card.name.given
+    nick << "_"
+    nick << card.name.family
+    nick.downcase
+  end
+    
 end
